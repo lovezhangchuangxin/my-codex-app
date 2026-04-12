cefaa6cf5c5366366562f1ef07c431d13d0b723b
lovekeqing <2911331070@qq.com>
2026-04-12 08:56:13 +0800

    refactor: remove unnecessary *.js files

--- packages/sdk/src/index.js
+++
@@ -1,3 +0,0 @@
-export { BridgeClient } from "./bridgeClient.js";
-export { BridgeThreadRuntime } from "./threadRuntime.js";
-export { createInitialSnapshot, findActiveTurnId, previewFromUserInput, toThreadDetail, toThreadSummary } from "./threadState.js";
